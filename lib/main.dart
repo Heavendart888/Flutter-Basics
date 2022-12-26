952e4364a1d42f860bff49c156e650b34f0322fe
KLH <mynameisbiswajit8@gmail.com>
2022-12-26 16:32:50 +0530

    Basics 4

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  void answerQuestion(){
+    print('Answer Chosen!');
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'what is your favorite colour?',
+      'what is your favorite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("My First App"),
+          ),
+          body: Column(
+            children: [
+              Text('The question!'),
+              ElevatedButton(
+                child: Text("Answer 1"),
+                onPressed: answerQuestion,
+              ),
+              ElevatedButton(
+                child: Text("Answer 2"),
+                onPressed: ()=>print("Answer 2 chosen"),
+              ),
+              ElevatedButton(
+                child: Text("Answer 3"),
+                onPressed: (){
+                  print("Answer 3 choosen");
+                },
+              ),
+            ],
+          )),
+    );
+  }
+}
